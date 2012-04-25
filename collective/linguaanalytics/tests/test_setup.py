056351fbf4edf02e37ca746eea2cd7e37d3e6481
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-04-25 13:28:03 +0200

    add test on browserlayer

--- collective/linguaanalytics/tests/test_setup.py
+++ collective/linguaanalytics/tests/test_setup.py
@@ -1,13 +1,29 @@
 import unittest2 as unittest
 from collective.linguaanalytics.tests import base
+from plone.browserlayer import utils
 
 class TestSetup(base.IntegrationTestCase):
     """We tests the setup (install) of the addons. You should check all
     stuff in profile are well activated (browserlayer, js, content types, ...)
     """
 
-    def test_xxx(self):
-        self.assertTrue(False)
+    def test_browserlayer(self):
+        from collective.linguaanalytics.interfaces import ILayer
+        layers = utils.registered_layers()
+        self.assertIn(ILayer, layers)
+
+class TestUninstall(base.IntegrationTestCase):
+    """Test if the addon uninstall well"""
+
+    def setUp(self):
+        super(TestUninstall, self).setUp()
+        qi = self.portal['portal_quickinstaller']
+        qi.uninstallProducts(products=['collective.linguaanalytics'])
+
+    def test_uninstall_browserlayer(self):
+        from collective.linguaanalytics.interfaces import ILayer
+        layers = utils.registered_layers()
+        self.assertNotIn(ILayer, layers)
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
